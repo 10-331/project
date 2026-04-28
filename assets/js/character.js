e9fc859506fc84fc1f74a58e6589d8c5fe636775
10-331 <9h8yxq5r4n@privaterelay.appleid.com>
2026-04-28 12:26:42 +0900

    Update character.js

--- assets/js/character.js
+++ assets/js/character.js
@@ -520,15 +520,3 @@ function init() {
 }
 
 init();
-
-if (characterBoard) {
-  characterBoard.addEventListener("click", (event) => {
-    if (!characterBoard.classList.contains("is-talk-active")) return;
-
-    event.stopPropagation();
-
-    document.querySelectorAll(".speech").forEach(el => {
-      el.style.opacity = "0";
-    });
-  });
-}
